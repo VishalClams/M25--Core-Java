3456e6e8dc3330ad90b0f16df9d124c56dbf5407
lenovo <lenovo@VishalPC.mshome.net>
2022-09-29 13:58:23 +0530

    first commit

---
+++ Day-1 _ConnectingEclipseWithGitHub/src/org/tnsindia/basic/TestingEclipseForGitHub.java
@@ -0,0 +1,10 @@
+package org.tnsindia.basic;
+
+public class TestingEclipseForGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello eclipse");
+	}
+
+}
