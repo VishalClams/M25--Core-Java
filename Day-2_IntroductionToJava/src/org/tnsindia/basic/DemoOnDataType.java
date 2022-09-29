9d0650602f6bd8fbcd570dd1bdede4b2d7bab340
lenovo <lenovo@VishalPC.mshome.net>
2022-09-29 14:07:20 +0530

    first commit

---
+++ Day-2_IntroductionToJava/src/org/tnsindia/basic/DemoOnDataType.java
@@ -0,0 +1,16 @@
+package org.tnsindia.basic;
+
+public class DemoOnDataType {
+
+	public static void main(String[] args) {
+		//byte xyz=128;
+				/*byte range is till 127 so 128 is out of range then
+				we have to go ahead with short/int or long*/
+				//compile time input
+				long xyz=128;
+				System.out.println(xyz);
+
+
+	}
+
+}
